3b24e550d0c5cdf49dc1e605e0a69d0da3d369cc
mbccastillo <miriam8cunha@gmail.com>
2021-11-23 19:10:10 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const path = require('path');
+const fs = require('fs');
+
+require("dotenv").config();
+
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+
+//app
+const app = express()
+
+//db
+mongoose
+.connect(process.env.MONGO_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+ })
+.then(() => console.log('DB Connected'));
+// .then((result) => app.listen(8000));
+ //.catch((err) => console.log(err));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+//routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is runnig on port ${port}`);
+
+});
---
+++ controllers/product.js
@@ -0,0 +1,49 @@
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
+const Product = require("../models/product");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+
+         // check for all fields
+         const { name, description, price, category, quantity, shipping } = fields;
+
+         if (!name || !description || !price || !category || !quantity || !shipping) {
+             return res.status(400).json({
+                 error: 'All fields are required'
+             });
+         }
+
+        let product = new Product(fields)
+
+        if(files.photo) {
+            console.log("FILES PHOTO: ", files.photo);
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    })
+}

a31dbb4fd9e9d57d2a60fce1708aa090f598435b
Miriam Castillo <miriam8cunha@gmail.com>
2021-11-27 14:56:00 -0800

    Update category.js

--- models/category.js
+++ models/category.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 
+
 const categorySchema = new mongoose.Schema({
     name: { 
         type: String,
         trim: true,
         required: true,
         maxlength: 32
+        //unique: true
         }
     }, 
         { timestamps: true }

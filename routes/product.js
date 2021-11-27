20c00c444ced356b453d158528dc83ecb702ab77
Miriam Castillo <miriam8cunha@gmail.com>
2021-11-27 14:57:26 -0800

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list} = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo, listSearch} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -16,8 +16,14 @@ router.put(
     update
 );
 
-
 router.get("/products", list);
+router.get("/products/search", listSearch);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
+
+
 router.param("userId", userById);
 router.param("productId", productById);
 

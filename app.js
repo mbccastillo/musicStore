f89b3a070dedf21703a77edef8ba6cbb3a3206eb
Miriam Castillo <miriam8cunha@gmail.com>
2021-11-27 14:58:01 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+//const cors = require('cors');
 const expressValidator = require("express-validator");
-//const path = require("path");
-//const fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 
 require("dotenv").config();
 
@@ -35,6 +36,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+//app.use(cors());
 
 //routes middleware
 app.use("/api", authRoutes);

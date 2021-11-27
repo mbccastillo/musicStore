6fd734d36082ed30f603c3fbd2f52f79df4ed23a
Miriam Castillo <miriam8cunha@gmail.com>
2021-11-27 14:57:05 -0800

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -7,17 +7,13 @@ const { userById } = require("../controllers/user");
 
 
 router.get("/category/:categoryId", read);
-
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create );
-
 router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update );
 
 router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove );
-
 router.get("/categories", list );
 
 router.param("categoryId", categoryById);
-
 router.param("userId", userById);
 
 module.exports = router;

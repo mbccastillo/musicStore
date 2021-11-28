ccf7ef7856f81a9ecfa1b95a249931e9b655050c
Miriam Castillo <miriam8castillo@gmail.com>
2021-11-28 03:21:20 -0800

    Add files via upload

--- back-end/models/category.js
+++ back-end/models/category.js
@@ -1,1 +1,19 @@
-category
+const mongoose = require('mongoose');
+
+
+
+const categorySchema = new mongoose.Schema({
+    name: { 
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+        //unique: true
+        }
+    }, 
+        { timestamps: true }
+);
+
+
+
+module.exports = mongoose.model("Category", categorySchema);

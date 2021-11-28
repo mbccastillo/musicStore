c543280fe5b2e2716c12aa106fa75fae2b6f824f
Miriam Castillo <miriam8castillo@gmail.com>
2021-11-28 03:25:47 -0800

    Add files via upload

--- front-end/src/core/Home.js
+++ front-end/src/core/Home.js
@@ -1,1 +1,12 @@
-asd
+import React from "react";
+import Layout from "./Layout";
+
+const Home = () => (
+<Layout title="Music Store" description="A treat to your ears!">
+    ...
+
+</Layout>
+
+);
+
+export default Home;

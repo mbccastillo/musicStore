1e30f0033be30ad7cfa53a8faf13589814e66799
Miriam Castillo <miriam8castillo@gmail.com>
2021-11-29 04:23:04 -0800

    Add files via upload

--- front-end/src/user/Signin.js
+++ front-end/src/user/Signin.js
@@ -1,10 +1,101 @@
-import React from "react";
+import React, {useState} from "react";
+import {Redirect} from "react-router-dom";
 import Layout from "../core/Layout";
+import {signin, authenticate, isAuthenticated} from "../auth";
 
-const Signin = () => (
-    <Layout title="Signin" description="Signin to our Music Store">
-        ...
-    </Layout>
-);
+const Signin = () => {
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        error: "",
+        loading: false,
+        redirectToReferrer: false
+    });
+
+    const {email, password, loading, error, redirectToReferrer } = values;
+    const {user} = isAuthenticated();
+
+    const handleChage = name => event => {
+        setValues({...values, error: false, [name]: event.target.value});
+    };
+
+ 
+
+     const clickSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false, loading: true });
+        signin({ email, password }).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, loading: false });
+            } else {
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        redirectToReferrer: true
+                    });
+                });
+            }
+        });
+    };
+ 
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChage("email")} type="email" className="form-control"
+                value={email}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChage("password")} type="password" className="form-control"
+                value={password}
+                />
+            </div>
+        <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+        </form>
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? "": "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showLoading = () => (
+        loading && (<div className="alert-info">
+            <h2>Loading...</h2>
+        </div>)
+    );
+
+    const redirectUser = () => {
+        if(redirectToReferrer) {
+           if(user && user.role === 1) {
+               return <Redirect to="/admin/dashboard" />
+           } else {
+               return <Redirect to="/user/dashboard" />
+           }
+        }
+        if(isAuthenticated()) {
+           return <Redirect to="/" />;
+        }
+    };
+
+    
+    return (
+        <Layout 
+            title="Signin" 
+            description="Signin to enter our Music Community"
+            className="container col-md-8 offset-md-2"
+            >
+            {showLoading()}
+            {showError()}
+            {signUpForm()}
+            {redirectUser()}
+        </Layout>
+    )
+    
+};
 
 export default Signin;
--- front-end/src/user/Signup.js
+++ front-end/src/user/Signup.js
@@ -1,72 +1,92 @@
 import React, {useState} from "react";
+import {Link} from "react-router-dom";
 import Layout from "../core/Layout";
-import { API } from "../config";
+import {signup} from "../auth";
 
 const Signup = () => {
     const [values, setValues] = useState({
-        name: '',
-        email: '',
-        password: '',
-        error: '',
+        name: "",
+        email: "",
+        password: "",
+        error: "",
         success: false
     });
 
-    const {name, email, password } = values;
+    const {name, email, password, success, error } = values;
 
     const handleChage = name => event => {
         setValues({...values, error: false, [name]: event.target.value});
     };
 
-     const signup = user => {
-         //console.log(name, email, password);
-         fetch(`${API}/signup`, {
-             method: "POST",
-             headers: {
-                 Accept: "application/json",
-                 "Content-Type": "application/json"
-             },
-             body:JSON.stringify(user)
-         })
-         .then(response => {
-             return response.json()
-         })
-         .catch (err => {
-             console.log(err);
-         })
-     };
+ 
 
-    const clickSubmit = (event) => {
+     const clickSubmit = event => {
         event.preventDefault();
-        signup({ name, email, password });
+        setValues({ ...values, error: false });
+        signup({ name, email, password }).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, success: false });
+            } else {
+                setValues({
+                    ...values,
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: "",
+                    success: true
+                });
+            }
+        });
     };
-
-   
-
+ 
     const signUpForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChage("name")} type="text" className="form-control"/>
+                <input onChange={handleChage("name")} type="text" className="form-control"
+                value={name}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChage("email")} type="email" className="form-control"/>
+                <input onChange={handleChage("email")} type="email" className="form-control"
+                value={email}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChage("password")} type="password" className="form-control"/>
+                <input onChange={handleChage("password")} type="password" className="form-control"
+                value={password}
+                />
             </div>
         <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
         </form>
-    )
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? "": "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{ display: success ? "" : "none" }}
+        >
+            New account is created. Please <Link to="/signin">Signin</Link>
+        </div>
+    );
+
+    
     return (
         <Layout 
             title="Signup" 
             description="Signup to be part of our Music Store Community"
             className="container col-md-8 offset-md-2"
             >
-                {signUpForm()}
-                {JSON.stringify(values)}
+            {showSuccess()}
+            {showError()}
+            {signUpForm()}
     </Layout>
     )
     

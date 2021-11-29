b9400c20726262f29d5e22fe6269fcada3303910
Miriam Castillo <miriam8castillo@gmail.com>
2021-11-29 04:23:29 -0800

    Add files via upload

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -3,16 +3,21 @@ import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
-
+import PrivateRoute from "./auth/PrivateRoute";
+import Dashboard from "./user/UserDashboard";
+import AdminRoute from "./auth/AdminRoute";
+import AdminDashboard from "./user/AdminDashboard";
 
 const Routes = () => {
     return (
+
         <BrowserRouter>
         <Switch>
             <Route path="/" exact  component={Home}/>
             <Route path="/signin" exact  component={Signin}/>
-            <Route path="/signup" exact  component={Signup}/>
-            
+            <Route path="/signup" exact  component={Signup}/>\
+            <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+            <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />            
         </Switch>
     </BrowserRouter>
     );

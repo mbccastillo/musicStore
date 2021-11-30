b94bd49ed93f124f6954ef66387bb52e1966a239
Peggy Roxy <roxypeggy43@gmail.com>
2021-11-29 16:16:09 -0800

    added style to nav

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(
+    <Routes />, 
+document.getElementById("root"));
 
 
